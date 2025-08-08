38f0424620bb7f64d762b8eeb8fc567a3f36c893
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2025-08-08 19:09:59 +0000

    Update gh-pages to output generated at 8c16995

---
+++ _next/static/chunks/pages/_error-5d6b64f1a914f021.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{2210:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(4519)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2210)}),_N_E=n.O()}]);
+//# sourceMappingURL=_error-5d6b64f1a914f021.js.map
---
+++ _next/static/chunks/pages/_offline-27f444848ca16c4d.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{2865:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return e(6241)}])},6241:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return r}});var t=e(4527),f=e.n(t),c=e(4084);function r(){return(0,c.useEffect)(()=>{f().replace(location.href)},[]),null}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2865)}),_N_E=n.O()}]);
+//# sourceMappingURL=_offline-27f444848ca16c4d.js.map
